9694463d5f9cf14f5cab846f09752dca110d700f
toshi <d-toshi@i.softbank.jp>
2022-10-31 12:42:33 +0900

    security

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from .settings_local import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
